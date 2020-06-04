02a210f903b78908f1fe2e2018167c110d3226e0
Majzur <nikodem.mazur@gmail.com>
2020-06-04 21:54:24 +0200

    Move persistence tests to new class and file

---
+++ Akka.NetTests/PersistentActorTests.cs
@@ -0,0 +1,62 @@
+﻿using Akka.Persistence.TestKit;
+using Xunit;
+
+namespace Akka.NetTests
+{
+    public class PersistentActorTests : PersistenceTestKit
+    {
+        [Fact]
+        public void ActorRecoversItsStateWithEventsWhenRestarted()
+        {
+
+        }
+
+        [Fact]
+        public void ActorRecoversItsStateAtOnceWithSnapshotWhenRestarted()
+        {
+
+        }
+
+        [Fact]
+        public void SenderOfReplayedMessageIsDeadLetters()
+        {
+
+        }
+
+        [Fact]
+        public void DeferAsyncDoesNotStoreEvents()
+        {
+
+        }
+
+        [Fact]
+        public void PoisonPillBreaksPersisting()
+        {
+
+        }
+
+        [Fact]
+        public void AtLeastOnceDeliveryActorGetsDeliveryConfirmationAfterBeingRestarted()
+        {
+
+        }
+
+        [Fact]
+        public void AtLeastOnceDeliveryActorSetsDeliverySnapshotDuringRecovery()
+        {
+
+        }
+
+        [Fact]
+        public void ActorStopsOnPersistFailure()
+        {
+
+        }
+
+        [Fact]
+        public void ActorIgnoresTheMessageAndContinuesWithTheNextOneOnPersistRejected()
+        {
+
+        }
+    }
+}
